f70b55bab6cb6fc7821d6fbe47967743207fa331
NVRC <nathaniel.charlebois@gmail.com>
2018-05-11 22:05:04 -0400

    Testing syntax

--- crossAnimate.py
+++ crossAnimate.py
@@ -4,11 +4,11 @@ import nLevelLinearGradient as lg
 if __name__ == '__main__':
     import sys
     color = []
-    color[0] = '#'+sys.argv[1]
-    color[1] = '#'+sys.argv[2]
-    period = int(sys.argv[3],10)
+    color1 = '#'+sys.argv[0]
+    color2 = '#'+sys.argv[1]
+    period = int(sys.argv[2],10)
     import gradientHelpers as gh
-    matrix = gh.getCrossColors(color[0],color[1],period)
+    matrix = gh.getCrossColors(color1,color2,period)
     columns = 60*period
     for i in range(0,columns):
         next = matrix[i][:]
