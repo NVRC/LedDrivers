260a4ec01284513173f9bccda53161eee52e8a20
NVRC <nathaniel.charlebois@gmail.com>
2018-05-11 22:07:42 -0400

    Tester

--- crossAnimate.py
+++ crossAnimate.py
@@ -3,15 +3,13 @@ import nLevelLinearGradient as lg
 
 if __name__ == '__main__':
     import sys
-    color = []
     color1 = '#'+sys.argv[0]
     color2 = '#'+sys.argv[1]
     period = int(sys.argv[2],10)
-    import gradientHelpers as gh
-    matrix = gh.getCrossColors(color1,color2,period)
+    color = lg.linear_gradient(color1,color2,60)
+    matrix = gh.getCrossColors(color,period)
     columns = 60*period
     for i in range(0,columns):
         next = matrix[i][:]
         lg.led_output(next)
         time.sleep(period)
-    pass
